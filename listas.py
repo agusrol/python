76a292a51ba30cb62783ae47e86e24cec4d0dfc5
Agustín Roldán <agusrol97@gmail.com>
2023-10-13 11:50:08 -0300

    Listas

---
+++ listas.py
@@ -0,0 +1,13 @@
+edades = [15,18,59,71];
+print(type(edades))
+edades.append(35);
+print(edades)
+edades.remove(18);
+edades.insert(1,18);
+print(edades);
+edades.clear();
+print(edades)
+edades.extend([2,3,4])
+print(edades)
+
+print(2 in edades)
